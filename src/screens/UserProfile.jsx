1f219ecbc35210efeb476680831665c2b9e3dee9
efsiqueira <edu.fss14@gmail.com>
2023-07-01 17:34:11 -0300

    Personalizando edição de usuário

--- src/screens/UserProfile.jsx
+++ src/screens/UserProfile.jsx
@@ -1,130 +1,89 @@
 import { View } from "react-native";
 import { Button, Text, TextInput } from "react-native-paper";
 import { useEffect, useState } from "react";
-import { doc, updateDoc } from "firebase/firestore";
+import { doc, updateDoc, deleteField } from "firebase/firestore";
 import { db } from "../config/firebase";
 import styles from "../utils/styles";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import UserProfileImagePicker from "./UserProfileImagePicker";
 
 export default function UserProfile() {
-    const [user, setUser] = useState(
-        {
-            nomeDaPessoa: "",
-            emailDaPessoa: "",
-            telefoneDaPessoa: "",
-            enderecoDaPessoa: "",
-            cepDaPessoa: "",
-            bairroDaPessoa: "",
-            cidadeDaPessoa: "",
-            estadoDaPessoa: "",
-        }
-    )
+  const [user, setUser] = useState({
+    nomeDaPessoa: "",
+    userUID: "",
+    image: null,
+  });
 
-    async function handleUpdate() {
-        try {
-            const usuarioId = user.userUID;
-            const docRef = doc(db, 'usuarios', usuarioId);
-            const updateTimestamp = await updateDoc(
-                docRef,
-                {
-                    nomeDaPessoa: user.nomeDaPessoa,
-                    emailDaPessoa: user.emailDaPessoa,
-                    telefoneDaPessoa: user.telefoneDaPessoa,
-                    bairroDaPessoa: user.bairroDaPessoa,
-                    cepDaPessoa: user.cepDaPessoa,
-                    cidadeDaPessoa: user.cidadeDaPessoa,
-                    estadoDaPessoa: user.estadoDaPessoa,
-                    userUID: user.userUID
-                }
-            );
-
-        } catch (error) {
-            console.error(error);
-        }
-
+  async function handleUpdate() {
+    try {
+      const usuarioId = user.userUID;
+      const docRef = doc(db, "usuarios", usuarioId);
+      const updateTimestamp = await updateDoc(docRef, {
+        nomeDaPessoa: user.nomeDaPessoa,
+        userUID: user.userUID,
+      });
+    } catch (error) {
+      console.error(error);
     }
+  }
 
-    useEffect(() => {
-        AsyncStorage.getItem("usuario")
-            .then(
-                (retorno) => {
-                    const usuario = JSON.parse(retorno)
-                    setUser(usuario)
-                }
-            )
+  async function handleDeleteImage() {
+    try {
+      const usuarioId = user.userUID;
+      const docRef = doc(db, "usuarios", usuarioId);
+      const updateTimestamp = await updateDoc(docRef, {
+        image: deleteField(),
+      });
+      setUser((prevUser) => ({
+        ...prevUser,
+        image: null,
+      }));
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-    }, [])
+  useEffect(() => {
+    AsyncStorage.getItem("usuario").then((retorno) => {
+      const usuario = JSON.parse(retorno);
+      setUser(usuario);
+    });
+  }, []);
 
-    return (
-        <View
-            style={styles.container}
+  return (
+    <View style={styles.container}>
+      <View style={styles.containerInner}>
+        <Text
+          style={{
+            ...styles.textCenter,
+            fontWeight: "bold",
+            marginBottom: 20,
+            color: "rgb(94, 143, 135)",
+          }}
+          variant="titleLarge"
         >
-            <View
-                style={styles.containerInner}
-            >
-                <Text
-                    style={
-                        {
-
-                            ...styles.textCenter,
-                            fontWeight: 'bold',
-                            marginBottom: 20
-                        }
-                    }
-                    variant="titleLarge"
-                >Edite seu perfil</Text>
-                <UserProfileImagePicker
-                    idUsuario={user.userUID}
-                />
-                <TextInput
-                    label="Nome"
-                    value={user.nomeDaPessoa}
-                    onChangeText={(text) => setUser(
-                        {
-                            ...user,
-                            nomeDaPessoa: text
-                        }
-                    )}
-                />
-                <TextInput
-                    label="E-mail"
-                    value={user.emailDaPessoa}
-                    onChangeText={(text) => setUser({ ...user, emailDaPessoa: text })}
-                    disabled={true}
-                />
-                <TextInput
-                    label="Telefone"
-                    value={user.telefoneDaPessoa}
-                    onChangeText={(text) => setUser({ ...user, telefoneDaPessoa: text })}
-                />
-                <TextInput
-                    label="CEP"
-                    value={user.cepDaPessoa}
-                    onChangeText={(text) => setUser({ ...user, cepDaPessoa: text })}
-                />
-                <TextInput
-                    label="Endereço"
-                    value={user.enderecoDaPessoa}
-                    onChangeText={(text) => setUser({ ...user, enderecoDaPessoa: text })}
-                />
-                <TextInput
-                    label="Cidade"
-                    value={user.cidadeDaPessoa}
-                    onChangeText={(text) => setUser({ ...user, cidadeDaPessoa: text })}
-                />
-                <TextInput
-                    label="Estado"
-                    value={user.estadoDaPessoa}
-                    onChangeText={(text) => setUser({ ...user, estadoDaPessoa: text })}
-                />
-
-                <Button
-                    mode="contained"
-                    style={{ maxWidth: 200, marginTop: 20, marginHorizontal: 'auto' }}
-                    onPress={handleUpdate}
-                >Atualizar meu perfil</Button>
-            </View>
-        </View >
-    )
+          Edite seu perfil
+        </Text>
+        <UserProfileImagePicker
+          idUsuario={user.userUID}
+          image={user.image}
+          onDeleteImage={handleDeleteImage}
+        />
+        <TextInput
+          label="Nome"
+          value={user.nomeDaPessoa}
+          onChangeText={(text) =>
+            setUser((prevUser) => ({ ...prevUser, nomeDaPessoa: text }))
+          }
+        />
+        <Button
+          mode="contained"
+          style={{ maxWidth: 200, marginTop: 20, marginHorizontal: "auto" }}
+          onPress={handleUpdate}
+        >
+          Atualizar meu perfil
+        </Button>
+      </View>
+    </View>
+  );
 }
--- src/screens/UserProfileImagePicker.jsx
+++ src/screens/UserProfileImagePicker.jsx
@@ -1,70 +1,80 @@
-import React, { useState, useEffect } from 'react';
-import { Image, View, Platform } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
-import { Button } from 'react-native-paper';
-import { Ionicons } from '@expo/vector-icons';
-import { addDoc, doc, getDoc, updateDoc } from 'firebase/firestore';
-import { db } from '../config/firebase';
+import React, { useState, useEffect } from "react";
+import { Image, View, Platform } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { Button } from "react-native-paper";
+import { Ionicons } from "@expo/vector-icons";
+import { updateDoc, doc } from "firebase/firestore";
+import { db } from "../config/firebase";
 
 export default function UserProfileImagePicker(props) {
-    const [image, setImage] = useState(null);
+  const [image, setImage] = useState(null);
 
-    useEffect(() => {
+  useEffect(() => {
+    if (props.image) {
+      setImage(props.image);
+    } else {
+      setImage(null);
+    }
+  }, [props.image]);
 
-        if (props.idUsuario == null) return;
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
 
-        getDoc(doc(db, 'usuarios', props.idUsuario))
-            .then(
-                (retorno) => {
-                    const usuario = retorno.data()
-                    setImage(usuario.image)
-                }
-            );
+    if (!result.cancelled) {
+      const docRef = doc(db, "usuarios", props.idUsuario);
+      const updateTimestamp = await updateDoc(docRef, {
+        image: result.uri,
+      });
 
+      setImage(result.uri);
+    }
+  };
 
-    }, [props.idUsuario])
+  const handleDeleteImage = async () => {
+    const docRef = doc(db, "usuarios", props.idUsuario);
+    const updateTimestamp = await updateDoc(docRef, {
+      image: null,
+    });
 
-    const pickImage = async () => {
-        // No permissions request is necessary for launching the image library
-        let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.All,
-            allowsEditing: true,
-            aspect: [1, 1],
-            quality: 1,
-        });
-        const docRef = doc(db, 'usuarios', props.idUsuario);
-        const updateTimestamp = await updateDoc(
-            docRef,
-            {
-                image: result.assets[0].uri
-            }
-        );
+    setImage(null);
+  };
 
-        if (!result.canceled) {
-            setImage(result.assets[0].uri);
-        }
-    };
-
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            {image && <Image
-                source={{ uri: image }}
-                style={
-                    {
-                        width: 100,
-                        height: 100,
-                        borderRadius: 100
-                    }
-                } />}
-            <Button
-                onPress={pickImage}
-
-            ><Ionicons
-                name="camera-outline"
-                size={24}
-                color="black"
-            >
-                </Ionicons></Button>
-        </View>
-    );
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      {image ? (
+        <Image
+          source={{ uri: image }}
+          style={{
+            width: 100,
+            height: 100,
+            borderRadius: 100,
+          }}
+        />
+      ) : (
+        <Ionicons
+          name="person-circle-outline"
+          size={100}
+          color="gray"
+          style={{
+            marginBottom: 10,
+          }}
+        />
+      )}
+      <View style={{ flexDirection: "row", marginTop: 10 }}>
+        <Button icon="camera" mode="outlined" onPress={pickImage}>
+          Escolher Imagem
+        </Button>
+        {image && (
+          <Button icon="delete" mode="outlined" onPress={handleDeleteImage}>
+            Excluir Imagem
+          </Button>
+        )}
+      </View>
+    </View>
+  );
 }
